9b8cc86a01cca0818204ac4ad43ba07957b5985b
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2024-12-17 08:15:36 +0000

    Create a sandwich using a helper method

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,18 +1,19 @@
 RSpec.describe 'An ideal sandwich' do
   Sandwich = Struct.new(:taste, :toppings)
 
-  # RSpec before hook, will run automatically before each example
-  before { @sandwich = Sandwich.new('delicious', []) }
+  def sandwich
+    @sandwich ||= Sandwich.new('delicious', [])
+  end
 
   it 'is delicious' do
-    taste = @sandwich.taste
+    taste = sandwich.taste
 
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    @sandwich.toppings << 'red peppers'
-    toppings = @sandwich.toppings
+    sandwich.toppings << 'red peppers'
+    toppings = sandwich.toppings
 
     expect(toppings).not_to be_empty
   end
