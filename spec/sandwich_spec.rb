94c43dfae60d29aea4ab905b11ae1d9cb00a2fcb
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2024-12-17 08:01:55 +0000

    Introduce sandwich spec without sharing setup
    Covers up to page 9.

---
+++ spec/sandwich_spec.rb
@@ -0,0 +1,23 @@
+RSpec.describe 'An ideal sandwich' do
+  Sandwich = Struct.new(:taste, :toppings)
+
+  it 'is delicious' do
+    # arrange: set up an object
+    sandwich = Sandwich.new('delicious', [])
+
+    # act: do something with it
+    taste = sandwich.taste
+ 
+    # assert: check that it behaved the way you wanted
+    expect(taste).to eq('delicious')
+  end
+
+  it 'lets me add toppings' do
+    sandwich = Sandwich.new('delicious', [])
+    sandwich.toppings << 'red peppers'
+    toppings = sandwich.toppings
+
+    # use not_to instead of to 
+    expect(toppings).not_to be_empty
+  end
+end
