cc5d7884de3aced9536d34f26a470797df977b58
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2024-12-30 09:01:10 +0000

    Use a before hook to set @sandwich
    Pages 9-11

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,22 +1,19 @@
 RSpec.describe 'An ideal sandwich' do
   Sandwich = Struct.new(:taste, :toppings)
 
-  it 'is delicious' do
-    # arrange: set up an object
-    sandwich = Sandwich.new('delicious', [])
-    # act: do something with it
-    taste = sandwich.taste
+  # RSpec before hook, will run automatically before each example
+  before { @sandwich = Sandwich.new('delicious', []) }
 
-    # assert: check that it behaved the way you wanted
+  it 'is delicious' do
+    taste = @sandwich.taste
+
     expect(taste).to eq('delicious')
   end
 
   it 'lets me add toppings' do
-    sandwich = Sandwich.new('delicious', [])
-    sandwich.toppings << 'red peppers'
-    toppings = sandwich.toppings
+    @sandwich.toppings << 'red peppers'
+    toppings = @sandwich.toppings
 
-    # use not_to instead of to 
     expect(toppings).not_to be_empty
   end
 end
