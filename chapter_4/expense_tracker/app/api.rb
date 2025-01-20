19c93f2ac8461b875d56f4361625c8eb553aebcc
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-01-20 09:32:49 +0000

    Implement get and post requests in api
    Expenses will persist in memory in `POST` request, so they can be
    retrieved, by date, in the `GET` request.

--- chapter_4/expense_tracker/app/api.rb
+++ chapter_4/expense_tracker/app/api.rb
@@ -1,14 +1,27 @@
 require 'sinatra/base'
 require 'json'
+require 'byebug'
 
 module ExpenseTracker
   class API < Sinatra::Base
+    def initialize
+      @expenses = []
+    end
+
     post '/expenses' do
-      JSON.generate('expense_id' => 42)
+      request.body.rewind
+      expense = JSON.parse(request.body.read)
+      expense['id'] = @expenses.size + 1
+      @expenses << expense
+
+      JSON.generate('expense_id' => expense['id'])
     end
 
     get '/expenses/:date' do
-      JSON.generate([])
+      date = params[:date]
+      expenses_for_date = @expenses.select { |expense| expense['date'] == date }
+
+      JSON.generate(expenses_for_date)
     end
   end
 end
