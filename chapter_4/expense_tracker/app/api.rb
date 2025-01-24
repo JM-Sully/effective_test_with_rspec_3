8888f659f06ec359dff481cca1bc6b66b00c8168
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-01-24 08:24:39 +0000

    Chapter 4 (#5)
    * Setup RSpec configuration for expense_tracker
    
    pages 47-48
    
    * Introduce expense_tracker api and spec
    
    Pages 49-52
    
    Verifies that the POST request completes without crashing the application;
    the barest skeleton of a Sinatra app.
    
    * Check the api response and body
    
    Pages 52 - 54
    
    * Introduce retrieving expense records by data
    
    Pages 55-57
    
    - Moves the post of an expense into a helper method `post_expense`.
    - Adds the api endpoint for retrieving records by date (thouh not
    implemented yet)
    - Uses the pending flag in the spec while we work on the logic to get
    this spec to pass
    
    * Install byebug gem
    
    This will allow me to use `debugger` while coding/fixing/implementing.
    
    * Implement get and post requests in api
    
    Expenses will persist in memory in `POST` request, so they can be
    retrieved, by date, in the `GET` request.
    
    * Hookup the app to a webserver
    
    Page 58
    
    Quite a few differences from the book here. I added the `rackup` and
    `webrick` gem to the gemfile and then ran `bundle` to install them. I
    had initially installed `puma` as the webserver, as it looks like the
    recommended choice based on the docs [1], but decided to use `webrick`
    to stay in line with the book.
    
    I started the server [2], ran the curl command [3], then had some
    template errors. These I fixed with `set :reload_templates, false`, and
    then everything ran well.
    
    I also tried running the commands in a more 'ruby-way' from `irb` [4],
    which also worked.
    
    [1] https://sinatrarb.com/intro.html
    [2] `bundle exec rackup`
    [3] `curl localhost:9292/expenses/2025-01-20 -w "\n"`
    
    [4]
    ```
    require 'net/http'
    require 'uri'
    
    uri = URI.parse("http://localhost:9292/expenses/2025-01-20")
    response = Net::HTTP.get_response(uri)
    puts response.body
    ```

---
+++ chapter_4/expense_tracker/app/api.rb
@@ -0,0 +1,31 @@
+require 'sinatra/base'
+require 'json'
+require 'byebug'
+
+module ExpenseTracker
+  class API < Sinatra::Base
+    configure do
+      set :reload_templates, false
+    end
+
+    def initialize
+      @expenses = []
+    end
+
+    post '/expenses' do
+      request.body.rewind
+      expense = JSON.parse(request.body.read)
+      expense['id'] = @expenses.size + 1
+      @expenses << expense
+
+      JSON.generate('expense_id' => expense['id'])
+    end
+
+    get '/expenses/:date' do
+      date = params[:date]
+      expenses_for_date = @expenses.select { |expense| expense['date'] == date }
+
+      JSON.generate(expenses_for_date)
+    end
+  end
+end
