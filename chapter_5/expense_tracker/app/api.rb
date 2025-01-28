de597458183ba444e9fa08631bc9e13c10747bcd
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-01-28 08:56:20 +0000

    Improve setup of api_spec
    Pages 67-69
    
    We'll eventually move the RecordResult class into the application code,
    but for now it can live in the spec.
    
    Using a simple value object like a RecordResult or Struct between layers
    makes it easier to isolate code and trust your tests. See Gary
    Bernhardt's 'Boundaries' talk [1] for more details.
    
    [1] https://www.destroyallsoftware.com/talks/boundaries

--- chapter_5/expense_tracker/app/api.rb
+++ chapter_5/expense_tracker/app/api.rb
@@ -9,6 +9,13 @@ module ExpenseTracker
       super()
     end
 
+    post '/expenses' do
+      request.body.rewind
+      expense = JSON.parse(request.body.read)
+      result = @ledger.record(expense)
+      JSON.generate('expense_id' => result.expense_id)
+    end
+
     class API < Sinatra::Base
       post '/expenses' do
         JSON.generate('expense_id' => 42)
