302324f2a604f5b0060b58c650fceb2c77f6ebcb
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-02-04 08:58:53 +0000

    Refactor specs in first context
    Page 72
    
    From the book:
    It may be tempting to DRY them up even further by also mocing the
    `post '/expenses...` lines into the `before` hook. Such a move would be
    overkill, though.
    
    Putting the `post` in the `before` hook will likely get in the way of
    future specs for this context. For instance, adding support for XML
    clients would require a different HTTP header. If you send the HTTP
    request from your setup code, you won't be able to twaek the headers for
    just your XML examples.

--- chapter_5/expense_tracker/spec/unit/app/api_spec.rb
+++ chapter_5/expense_tracker/spec/unit/app/api_spec.rb
@@ -14,13 +14,15 @@ module ExpenseTracker
     let (:ledger) { instance_double('ExpenseTracker::Ledger') }
 
     context 'when the expense is successfully recorded' do
-      it 'returns the expense id' do
-        expense = { 'some' => 'data' }
+      let (:expense) { { 'some' => 'data' } }
 
+      before do
         allow(ledger).to receive(:record)
           .with(expense)
           .and_return(RecordResult.new(true, 417, nil))
+      end
 
+      it 'returns the expense id' do
         post '/expenses', JSON.generate(expense)
 
         parsed = JSON.parse(last_response.body)
@@ -28,12 +30,6 @@ module ExpenseTracker
       end
 
       it 'responds with a 200 OK' do
-        expense = { 'some' => 'data' }
-
-        allow(ledger).to receive(:record)
-          .with(expense)
-          .and_return(RecordResult.new(true, 417, nil))
-
         post '/expenses', JSON.generate(expense)
         expect(last_response.status).to eq(200)
       end
