0e799fa5168eb73c0010d25907d2cbb251d3cc06
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-02-07 08:57:07 +0000

    Refactor parsing JSON into a let block
    Exercise 1, page 78
    
    I first did this with a helper method, but I prefer using a let block
    when the logic isn't too complicated.

--- chapter_5/expense_tracker/spec/unit/app/api_spec.rb
+++ chapter_5/expense_tracker/spec/unit/app/api_spec.rb
@@ -12,6 +12,7 @@ module ExpenseTracker
     end
 
     let (:ledger) { instance_double('ExpenseTracker::Ledger') }
+    let (:parsed) { JSON.parse(last_response.body)}
 
     context 'when the expense is successfully recorded' do
       let (:expense) { { 'some' => 'data' } }
@@ -25,7 +26,6 @@ module ExpenseTracker
       it 'returns the expense id' do
         post '/expenses', JSON.generate(expense)
 
-        parsed = JSON.parse(last_response.body)
         expect(parsed).to include('expense_id' => 417)
       end
 
@@ -47,7 +47,6 @@ module ExpenseTracker
       it 'returns an error message' do
         post '/expenses', JSON.generate(expense)
 
-        parsed = JSON.parse(last_response.body)
         expect(parsed).to include('error' => 'Expense Incomplete')
       end
 
