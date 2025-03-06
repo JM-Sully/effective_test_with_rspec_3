b0481855cd9afe0169692cc811b02af31bebc48e
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-03-06 08:52:18 +0000

    Refactor shared examples into shared_examples.rb
    pages 128-131

--- chapter_7/expense_tracker/spec/exercises/uri_spec.rb
+++ chapter_7/expense_tracker/spec/exercises/uri_spec.rb
@@ -1,13 +1,8 @@
 require 'uri'
+require 'support/shared_examples'
 
 RSpec.describe URI do
-  it 'parses the host' do
-    expect(URI.parse('http://foo.com/').host).to eq 'foo.com'
-  end
-
-  it 'parses the port' do
-    expect(URI.parse('http://example.com:9876').port).to eq 9876
-  end
+  it_behaves_like 'a URI parser', URI
 
   it 'defaults the port for an http URI to 80' do
     expect(URI.parse('http://example.com/').port).to eq 80
