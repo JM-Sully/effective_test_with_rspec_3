d0c4ccab076aaa0e764f61deba8fa3f31f0dd68c
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-03-11 07:39:47 +0000

    Define 'model' metadata for running models
    page 135-136
    
    RSpec will add the model metadata tag to all specs tagged with type: :model.
    
    This will allow you to run all model specs with the command at [1].
    
    [1] rspec --tag model

--- chapter_8/expense_tracker/spec/spec_helper.rb
+++ chapter_8/expense_tracker/spec/spec_helper.rb
@@ -111,4 +111,8 @@ RSpec.configure do |config|
   config.define_derived_metadata(file_path: /spec\/unit/) do |meta|
     meta[:fast] = true
   end
+
+  config.define_derived_metadata(type: :model) do |meta|
+    meta[:models] = true
+  end
 end
