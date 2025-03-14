7fdc847b9e860e42e1f94a339b73164ee4c8220e
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-03-14 10:14:19 +0000

    Setup code for chapter 9

---
+++ chapter_9/expense_tracker/app/ledger.rb
@@ -0,0 +1,25 @@
+require_relative '../config/sequel'
+
+module ExpenseTracker
+  RecordResult = Struct.new(:success?, :expense_id, :error_message)
+
+  class Ledger
+    def record(expense)
+      required_keys = ['payee', 'amount', 'date']
+      missing_keys = required_keys - expense.keys
+
+      unless missing_keys.empty?
+        message = "Invalid expense, missing: #{missing_keys.join(', ')}"
+        return RecordResult.new(false, nil, message)
+      end
+
+      DB[:expenses].insert(expense)
+      id = DB[:expenses].max(:id)
+      RecordResult.new(true, id, nil)
+    end
+
+    def expenses_on(date)
+      DB[:expenses].where(date: date).all
+    end
+  end
+end
