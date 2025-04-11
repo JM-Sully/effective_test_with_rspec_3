60ed8f1f089f93fc4f4d39a9c471f6cfd736fb02
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-04-11 08:43:01 +0100

    Chapter 11 (#13)
    * Add starting code for exercises from the book
    
    * Use yield_successive_args to improve phone number spec
    
    * Use change and have_attributes in public company spec
    
    * Use start_with to improve tokenizer spec
    
    * Use contain_exactly to improve water spec
    
    * Use dynamic predicate to improve calendar spec

---
+++ chapter_11/public_company_spec.rb
@@ -0,0 +1,28 @@
+PublicCompany = Struct.new(:name, :value_per_share, :share_count) do
+  def got_better_than_expected_revenues
+    self.value_per_share *= rand(1.05..1.10)
+  end
+
+  def market_cap
+    @market_cap ||= value_per_share * share_count
+  end
+end
+
+RSpec.describe PublicCompany do
+  let(:company) { PublicCompany.new('Nile', 10, 100_000) }
+
+  it 'increases its market cap when it gets better than expected revenues' do
+    expect do
+      company.got_better_than_expected_revenues
+    end.to change { company.market_cap }.by_at_least(50_000)
+  end
+
+  it 'provides attributes' do
+    expect(company).to have_attributes(
+      name: 'Nil',
+      value_per_share: 10,
+      share_count: 10_000,
+      market_cap: 1_000_000
+    )
+  end
+end
