91f6bb769dff474081aba3ecd3c8242ca22e6432
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-04-28 07:57:55 +0100

    Use the class name, rather than a string
    My preference is to use the class name, rather than a string, when
    creating a new instance of a class. This is because it makes the code
    easier to read and understand, and it also helps to avoid potential
    errors that can occur if the class name is changed in the future.

--- chapter_13/mountain/spec/skier_spec.rb
+++ chapter_13/mountain/spec/skier_spec.rb
@@ -3,7 +3,7 @@ require 'skier'
 module Mountain
   RSpec.describe Skier do
     it 'gets tired after skiing a difficult slope' do
-      trail_map = instance_double('TrailMap', difficulty: :expert)
+      trail_map = instance_double(Mountain::TrailMap, difficulty: :expert)
 
       skier = Skier.new(trail_map)
       skier.ski_on('Last Hoot')
