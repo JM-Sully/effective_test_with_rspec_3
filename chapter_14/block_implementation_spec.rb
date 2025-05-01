f7f2c9cb050d425d689652d870297151e460cdca
Jess Sully <45972923+JM-Sully@users.noreply.github.com>
2025-05-01 08:04:39 +0100

    Implement RSpec's interface for test doubles, rather than blocks

--- chapter_14/block_implementation_spec.rb
+++ chapter_14/block_implementation_spec.rb
@@ -2,33 +2,25 @@ RSpec.describe "Block implementations that provide responses" do
   let(:test_double) { double }
 
   it "can return a value" do
-    allow(test_double).to receive(:message) do
-      17
-    end
+    allow(test_double).to receive(:message).and_return(17)
 
     expect(test_double.message).to eq(17)
   end
 
   it "can raise an error" do
-    allow(test_double).to receive(:message) do
-      raise(StandardError, 'boom')
-    end
+    allow(test_double).to receive(:message).and_raise(StandardError, 'boom')
 
     expect { test_double.message }.to raise_error(/boom/)
   end
 
   it "can yield a value" do
-    allow(test_double).to receive(:message) do |&block|
-      block.call(1)
-    end
+    allow(test_double).to receive(:message).and_yield(1)
 
     expect { |b| test_double.message(&b) }.to yield_with_args(1)
   end
 
   it "can throw a symbol" do
-    allow(test_double).to receive(:message) do
-      throw :foo
-    end
+    allow(test_double).to receive(:message).and_throw(:foo)
 
     expect { test_double.message }.to throw_symbol(:foo)
   end
@@ -38,42 +30,29 @@ RSpec.describe "Block implementations that check calls" do
   let(:test_double) { double }
 
   it "can constrain arguments" do
-    allow(test_double).to receive(:message) do |arg|
-      raise('invalid_arg') if arg == :invalid_arg
-    end
+    allow(test_double).to receive(:message)
+    allow(test_double).to receive(:message).with(:invalid_arg).and_raise('invalid_arg')
 
     expect { test_double.message(:valid_arg) }.not_to raise_error
     expect { test_double.message(:invalid_arg) }.to raise_error(/invalid_arg/)
   end
 
   it "can count how many times the message was received" do
-    receive_count = 0
-
-    allow(test_double).to receive(:message) do |&block|
-      receive_count += 1
-    end
+    allow(test_double).to receive(:message)
 
     test_double.message
     test_double.message
 
-    expect(receive_count).to eq(2)
+    expect(test_double).to have_received(:message).twice
   end
 
   it "can constrain the order messages were received in" do
-    sequence = []
-
-    allow(test_double).to receive(:message_1) do
-      sequence << :message_1
-    end
-
-    allow(test_double).to receive(:message_2) do
-      sequence << :message_2
-    end
+    expect(test_double).to receive(:message_1).ordered
+    expect(test_double).to receive(:message_2).ordered
+    expect(test_double).to receive(:message_1).ordered
 
     test_double.message_1
     test_double.message_2
     test_double.message_1
-
-    expect(sequence).to eq([:message_1, :message_2, :message_1])
   end
 end
